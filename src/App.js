d28ac9a628a706a0a3e1b670a6bfeecad4847828
gustavdias <gustav.almd@gmail.com>
2021-02-16 10:20:46 +0300

    add base for auth app with material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import SignUp from "./containers/SignUp/SignUp";
 import LogIn from "./containers/LogIn/LogIn";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { Route, Switch } from "react-router-dom";
 import UserInfo from "./containers/UserInfo/UserInfo"
 import Logout from "./containers/Logout/Logout"
 
 export class App extends Component {
   render() {
-    const routes = (
+    let routes = (
       <Switch>
         <Route path="/signup" component={SignUp} />
         <Route path="/" component={LogIn} />
@@ -16,8 +16,8 @@ export class App extends Component {
     );
 
     //if user auth
-    if (this.props.isAuthenticated) {
-      const routes = (
+    if (false) {
+      routes = (
         <Switch>
           <Route path="/logout" component={Logout} />
           <Route path="/" component={UserInfo} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import useStyles from "./NavBarStyle";
+import { Link } from "react-router-dom";
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            UserInfo App
+          </Typography>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Sign Up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
